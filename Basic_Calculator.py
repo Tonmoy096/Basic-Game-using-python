239f1f84246de1aabb8ba7f18659342e807cd2c9
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-24 19:28:39 +0600

    refactor calculator function to improve input handling and user interaction

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -21,4 +21,28 @@ def calculator():
 
     while True:
         choice = input("Enter choice 1/2/3/4:")
-         
+
+        if choice in ["1","2","3","4"]:
+            try:
+                num1 = float(input("Enter first number:"))
+                num2 = float(input("Enter second number:"))
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+                continue
+                
+            if choice == "1":
+                print(num1,"+",num2,"=",add(num1,num2) )
+            elif choice == "2":
+                print(num1,"-", num2, "=", subtract(num1,num2))
+            elif choice == "3":
+                print(num1,"*", num2, "=", multiply(num1,num2))
+            elif choice == "4":
+                print(num1, "/", num2, '=', divide(num1,num2))
+
+            next_calculation = input("Do you want to perform another calculation? (yes/no):")
+            if next_calculation.lower() != "yes":
+                break
+        else:
+            print("Invalid input. Please enter valid choice between 1 and 4")
+if __name__ == "__main__":
+  calculator()
